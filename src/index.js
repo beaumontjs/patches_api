d479b24a9589d932279fef51f507432840015166
Glowyrm <Glowyrm.prime@gmail.com>
2016-10-24 21:07:59 -0500

    set up initial Grant middleware settings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
 import express from 'express';
-import router from './routes/index.js'
+import router from './routes/index.js';
+import Grant from 'grant';
 
+const grantMiddle = new Grant(require(./oauthconfig.json));
 const app = express();
 
+app.use(grantMiddle);
 app.use(router);
 app.listen(process.env.PORT || 8080);
 
