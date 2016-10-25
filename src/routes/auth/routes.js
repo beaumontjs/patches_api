a47781446b66f03e22badc5a32be4c328c29d99c
Joah Gerstenberg <me@joahg.com>
2016-10-24 20:35:50 -0500

    add callback_uri to twitter auth route

--- src/routes/auth/routes.js
+++ src/routes/auth/routes.js
@@ -4,7 +4,8 @@ export default function(req, res) {
   res.send(JSON.stringify([
     {
       type: 'twitter',
-      uri: 'https://api.twitter.com/outh2'
+      uri: 'https://api.twitter.com/outh2',
+      callback_uri: 'http://' + req.headers.host + '/auth/twitter'
     }
   ]))
 }
